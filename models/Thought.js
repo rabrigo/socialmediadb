6370b85d61cc85c0977db2ea3ce6ea85d3135924
Raw Ness <rawness@Raws-Mac-mini.local>
2022-02-27 13:24:14 -0800

    new upload

--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,34 @@
 const { Schema, model } = require('mongoose');
-
-const thoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        unique: true,
-        trim: true,
-        required: true
+const reactionSchema = require('./Reaction');
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false
     }
-    createdAt: {
-        type: Date,
-        default: Date.now,
-      },
+)
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 })
 
+const Thought = model('thought', thoughtSchema);
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,21 +1,47 @@
 const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought')
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-        trim: true,
-        required: true
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Email validation failed'
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: this
+            }
+        ]
     },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        validate: {
-            validator: () => Promise.resolve(false),
-            message: 'Email validation failed'
-          }
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false
     }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
 })
 
+const User = model('user', userSchema);
 module.exports = User;
